0fe7709171b31512047638d5d0b23d96552efb9c
Sen2017 <casintahan47at@gmail.com>
2017-01-11 09:20:47 -1000

    fortune list added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
-console.log("js linked");
+console.log("app.js linked");
 
 function generateFortuneCookie() {
 
-	var fortunes = ["You will die", "You will not die... yet", "Edsen is the best", "A Husky will eat you", "A Nykosaur will maul you... and then eat you", "A beetle gets squished", "You will pay Edsen P. Casintahan $1.00 USD for his birthday, if its after his birthday (January 06) you will pay $100.00 USD", "You win! Go tell your teacher", "You lose! Go tell your mom", "You see a ghostly beetle. He asks you WHY YOU LET ME DIE"];
-	var changeDiv = document.getElementByID("fortuneCookieText"); 
-	changeDiv.innerHTML = fortunes[5];
-	console.log("CHECK");
+	var fortunes = ["You will die.", "You will not die... yet.", "I don't know about your future, but I feel good.", "A Husky will eat you.", "A Nykosaur will maul you... and then eat you.", "A beetle gets squished.", "You will pay Edsen P. Casintahan $1.00 USD for his birthday, if its after his birthday (January 06) you will pay $100.00 USD.", "You win! Go tell your teacher.", "You lose! Go tell your mom...", "You'll see a ghostly beetle. He will ask you WHY YOU LET ME DIE. selfish human..."];
+	var fortuneDraw = fortunes[Math.floor((Math.random() * 10) + 1)];
+	var node = document.createElement("li");
+	var fortuneCookieText = document.getElementById("fortuneCookieText");
+	var textNode;
 
+	if (fortuneDraw == undefined) {
+
+		var textNode = document.createTextNode("Sorry, your future isn't clear. Check your wifi...");
+
+	}else{
+
+		var textNode = document.createTextNode(fortuneDraw);
+
+	}
+
+	node.appendChild(textNode);
+	document.getElementById("previousFortunes").appendChild(node);
+
+	console.log("Generated fortune");
 }
