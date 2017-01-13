aa2b8593a675596bce8c0a350c0556bbb0c9ec95
Sen2017 <casintahan47at@gmail.com>
2017-01-13 09:00:14 -1000

    complete!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 console.log("app.js linked");
 
+
+
 function generateFortuneCookie() {
 
 	var fortunes = ["You will die.", "You will not die... yet.", "I don't know about your future, but I feel good.", "A Husky will eat you.", "A Nykosaur will maul you... and then eat you.", "A beetle gets squished.", "You will pay Edsen P. Casintahan $1.00 USD for his birthday, if its after his birthday (January 06) you will pay $100.00 USD.", "You win! Go tell your teacher.", "You lose! Go tell your mom...", "You'll see a ghostly beetle. He will ask you WHY YOU LET ME DIE. selfish human..."];
@@ -20,6 +22,7 @@ function generateFortuneCookie() {
 
 	node.appendChild(textNode);
 	document.getElementById("previousFortunes").appendChild(node);
+	fortuneCookieText.innerHTML = fortuneDraw;
 
 	console.log("Generated fortune");
 }
